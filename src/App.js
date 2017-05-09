f54cacdadd83c0aec0dd83233d46c7148f501b27
RDP <rdupaw@gmail.com>
2017-05-09 12:46:34 -0500

    Initial commit for challange

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var PropsDisplayer = React.createClass({
+  render: function () {
+    var stringProps = JSON.stringify(this.props);
+
+    return (
+      <div>
+        <h1>CHECK OUT MY PROPS OBJECT</h1>
+        <h2></h2>
+      </div>
+    );
+  }
+});
+
+
