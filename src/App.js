b43a524700d906bf83cf73e7bf0302028768f27f
RDP <rdupaw@gmail.com>
2017-05-09 13:18:24 -0500

    Change singedIn to true

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ var App = React.createClass({
         <h1>
           Hullo and, "Welcome to The Newzz," "On Line!"
         </h1>
-        <Greeting name="RDP" signedIn={false} />
+        <Greeting name="RDP" signedIn={true} />
         <article>
           Latest: where is my phone?
         </article>
